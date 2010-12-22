8624ae0038da9c419275282552f26a6e561d5f91
Joshua Abbott <joshnabbott@gmail.com>
2010-12-21 22:48:25 -0700

    adding rotating header

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,2 +1,12 @@
 $(document).ready(function() {
+  setTimeout(function(){
+    $('#slider').nivoSlider({
+      effect: 'fade',
+      directionNav: false,
+      pauseTime: 5000,
+      pauseOnHover: false,
+      controlNavThumbs: true,
+      controlNavThumbsReplace: '-thumb.jpg'
+    });
+  }, 2000);
 });
